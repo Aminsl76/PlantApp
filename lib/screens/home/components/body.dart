75792e3808c7cc98129b710e517e139ee8623af3
Aminsl76 <aminsoley208@gmail.com>
2022-08-31 13:11:22 +0430

    plants list items creating

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/constants.dart';
 
+import 'header_with_search_box.dart';
+
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
@@ -9,86 +11,109 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Column(
-      children: [
-        HeaderWithSearchBox(size: size)
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recommended", press: (){},) ,
+          Container(
+            width: size.width * 0.4,
+            margin: EdgeInsets.only(
+              left: kDefaultPadding,
+              top: kDefaultPadding / 2,
+              bottom: kDefaultPadding * 2.5
+            ),
+            child: Column(
+              children: [
+                Image.asset("assets/images/image_1.png"),
+                Container(
+                  padding: EdgeInsets.all(kDefaultPadding / 2),
+                  decoration: BoxDecoration(
+                    color: Colors.white ,
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: kPrimaryColor.withOpacity(0.23)
+                      )
+                    ]
+                  ),
+                ) ,
+                Row(
+                  children: [
+                    RichText(text: TextSpan(
+                      children: [
+
+                      ]
+                    ))
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
-    Key? key,
-    required this.size,
+class TitleWithMoreBtn extends StatelessWidget {
+  const TitleWithMoreBtn({
+    Key? key, required this.title, required this.press,
   }) : super(key: key);
 
-  final Size size;
+  final String title;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: [
+          TitleWithCustomUnderline(title: title),
+          Spacer(),
+          MaterialButton(
+            onPressed: (){},
+            color: kPrimaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Text("More" , style: TextStyle(color: Colors.white)),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({Key? key, required this.title})
+      : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: size.height * 0.2,
+      height: 24,
       child: Stack(
         children: [
-          Container(
-            padding: EdgeInsets.only(
-              left: kDefaultPadding ,
-              right: kDefaultPadding ,
-              bottom: 36 + kDefaultPadding
+          Padding(
+            child: Text(
+              "Recommended",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            height: size.height * 0.2 - 27,
-            decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36))),
-            child: Row(
-              children: [
-                Text("Hi Uishopy",
-                    style: Theme.of(context).textTheme.headline5?.copyWith(
-                        color: Colors.white, fontWeight: FontWeight.bold)
-                ) ,
-                Spacer(),
-                Image.asset("assets/images/logo.png")
-              ],
-            ),
+            padding: EdgeInsets.only(left: kDefaultPadding / 4),
           ),
           Positioned(
             bottom: 0,
             left: 0,
             right: 0,
             child: Container(
-              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              alignment: Alignment.center,
-              height: 54,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23))
-                  ]),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        hintStyle: TextStyle(
-                            color: kPrimaryColor.withOpacity(0.5)),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
-                      onChanged: (value) {},
-                    ),
-                  ),
-                  SvgPicture.asset("assets/icons/search.svg")
-                ],
-              ),
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
             ),
           )
         ],
